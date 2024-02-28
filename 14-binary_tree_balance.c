e28a8dd5043691e9fe76c3170dd3df647c6fd356
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-02-28 19:35:01 +0200

    mand

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,11 +23,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t l = 0, r = 0;
+		size_t xl = 0, xr = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? l : r);
+		xl = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		xr = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((xl > xr) ? xl : xr);
 	}
 	return (0);
 }
