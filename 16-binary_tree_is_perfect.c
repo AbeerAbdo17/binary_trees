5222dfb87a161ea5aff804614982f7fb9be00fca
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2024-02-28 20:23:20 +0200

    mand

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,12 +10,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t xleft_height, xright_height;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	xleft_height = binary_tree_height(tree->left);
 	xright_height = binary_tree_height(tree->right);
 
-	return (xleft_height > xright_height ? xleft_height : xright_height) + 1;
+	return ((xleft_height > xright_height ? xleft_height : xright_height) + 1);
 }
 
 /**
@@ -26,9 +26,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
-	return binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
 /**
@@ -42,11 +42,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		size_t height, size;
 
 		if (tree == NULL)
-			return 0;
+			return (0);
 
 		height = binary_tree_height(tree);
 		size = binary_tree_size(tree);
 
-		return (size == ((size_t)1 << height) - 1);
+		return ((size == ((size_t)1 << height) - 1));
 	}
 }
